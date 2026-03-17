7b8e575047d1ef1815396a89b74868c6329068f9
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2026-03-17 19:30:36 +0100

    Implement a very basic web-based dashboard

--- src/aquifer/command_line/__init__.py
+++ src/aquifer/command_line/__init__.py
@@ -1,6 +1,10 @@
 """Command-line interface for the Aquifer application."""
 
+from typing import Annotated
+
 import typer
+
+from aquifer.dashboard import app as dashboard_app
 
 from . import meter, rain, tank
 
@@ -9,3 +13,11 @@ app = typer.Typer(help="Rainwater tank storage estimation using rainfall data an
 app.add_typer(meter.app, name="meter")
 app.add_typer(rain.app, name="rain")
 app.add_typer(tank.app, name="tank")
+
+
+@app.command()
+def dashboard(
+    debug: Annotated[bool, typer.Option("--debug", help="Run the dashboard in debug mode.")] = False,
+) -> None:
+    """Start the dashboard."""
+    dashboard_app.run(debug=debug)
---
+++ src/aquifer/dashboard.py
@@ -0,0 +1,38 @@
+
+import dash_mantine_components as dmc
+from dash import Dash
+
+from aquifer.configuration import Configuration
+from aquifer.estimation import Estimation
+
+
+def serve_layout():
+    config = Configuration.find()
+    estimation = Estimation(config)
+
+    level = estimation.storage["level"].iloc[-1] * 100
+    storage = estimation.storage["storage"].iloc[-1]
+    capacity = config.tank.capacity
+
+    return dmc.MantineProvider(
+        dmc.AppShell(
+            [
+                dmc.AppShellHeader(
+                    dmc.Group(
+                        [
+                            dmc.Title("Aquifer", c="blue"),
+                        ],
+                        h="100%",
+                        px="md",
+                    )
+                ),
+                dmc.AppShellMain(f"{level:.0f} % ({storage:.0f} / {capacity:.0f})"),
+            ],
+            header={"height": 60},
+            padding="md",
+        )
+    )
+
+
+app = Dash(title="Aquifer")
+app.layout = serve_layout
